d369b9ae9d4c6b60b25679d9f4355772fefb3e08
unknown <nishanth.t40@gmail.com>
2025-11-12 23:24:03 +0530

    Created a React App using Parcel Bundler

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // First React Code. 
 const heading = React.createElement("h1",{id:"heading", class: "header"}, "Namaste React (children)");
 const Root = ReactDOM.createRoot(document.getElementById("root"));
