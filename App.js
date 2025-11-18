6ee823278a315380ee3e3c2d0c083faa81bfae60
unknown <nishanth.t40@gmail.com>
2025-11-18 22:13:11 +0530

    JSX ReactElement and Functional Components

--- App.js
+++ App.js
@@ -1,15 +1,103 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// First React Code. 
-const heading = React.createElement("h1",{id:"heading", class: "header"}, "Namaste React (children)");
-const Root = ReactDOM.createRoot(document.getElementById("root"));
-// Root.render(heading);
 
-// Nested Elements: 
-const parent = React.createElement("div",{id:'parent'}, //parent
-    React.createElement("div",{id:'child'}, //child
-    [React.createElement("h1",{},'First Element'),React.createElement("h2",{},'Second Element')]) //Siblings
-); 
-Root.render(parent);
+// First React Code. 
+// const heading = React.createElement("h1",{id:"heading", class: "header"}, "Namaste React (children)");
+const Root = ReactDOM.createRoot(document.getElementById("root"));
+// // // Root.render(heading);
+
+// // Nested Elements: 
+// const parent = React.createElement("div",{id:'parent'}, //parent
+//     React.createElement("div",{id:'child'}, //child
+//     [React.createElement("h1",{},'First Element'),React.createElement("h2",{},'Second Element')]) //Siblings
+// ); 
+// Root.render(parent);
+
+// JSX (Javascript XML) (This is not HTML, But this is HTML Like Syntax). 
+//Compilation is done by Babel which is a Parcel Package inside Node Module. 
+
+const jsxheading = (<h1 id="Heading" tabIndex={5} className="HeaderName">Namaste React in JSX</h1>,
+    <h2>This is Nishanth T</h2>
+);
+// Root.render(jsxheading);
+
+// React Components: 
+// There are 2 ways to create a React Components. 
+// Class based Component (Older way) and Functional Component (New/Modern Way)
+
+// React Functional Component: (This is a Normal JS Function which will return Some piece pf JSX Code,
+// This is a Function that Returns a React Element)
+
+const Title = () =>(
+    <h1>This is a Title function</h1>
+);
+
+const Reactelem = (<h1>This is a Element</h1>);
+
+const number = 1000; 
+
+const HeaderComponent = () => (
+<div id="Container">
+{/* Component Inside the Component */}
+{/* <Title></Title> */}
+{Title()}
+{/* React Element or Plain JS inside Component */}
+{/* {number}
+{console.log("NamasteJS")}
+<h2>{number}</h2>
+{100+200} */}
+<h1>React Functional Component</h1>
+{Reactelem}
+</div>);
+
+// Root.render(Title()); 
+
+// Coding Assignments: (Chapter 03 - Laying the Foundation): 
+// Using Reactelement
+
+const Reactelement = React.createElement("div",{className:"title"},[
+    React.createElement("h1",{key: "1"},"1st Element"), React.createElement("h2",{key: "2"},"2nd Element"), 
+    React.createElement("h3",{key: "3"},"3rd Element")
+]);
+
+// Using JSX: 
+const JsxReactelement = (
+     <div className="title">
+        <h1>1st element Comp</h1>
+        <h2>2nd element Comp</h2>
+        <h3>3rd element Comp</h3>
+    </div>
+);
+// Using Functional Component: 
+const FunctionComp = () => (
+    <>
+        {Reactelement}
+    </>
+);
+// Root.render(jsxReactelement);
+// Root.render(Reactelement);
+// Root.render(<FunctionComp></FunctionComp>)
+
+// Create a Header Component from scratch using Functional Components with JSX: 
+
+const Header = () => (
+    <div className="header">
+        <div className="logo">
+             <span className="Logo-icon">Namaste React</span>
+        </div>
+        <div className="searchbar">
+            <input placeholder="Search" type="text" />
+        </div>
+        <div className="user-icon">
+            <span className="icon">N</span>
+        </div>
+    </div>
+);
+
+Root.render(Header());
+
+
+
+
 
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-This is a React Learning Course. 
+This is a React Learning Course: 
+
+1. Basic Understanding of Creating Elements in HTML,Through JS and in ReactJS.
+2. Installing Parcel Bundler. (npm)
+3. Creating ReactElements and Understanding of Package,json and Package.lock.json. 
+4. What is npm and Node Modules.  
+5. Installing React Module. 
+6. Executing/Building Parcel in Local and Produnction Mode. (npx)  
+7. Adding the Run Commands in Script Section in Package.json
+8. Understanding of JSX ReactElements. 
+9. Understanding of Babel 
+10. React Components. (Functional (New/Modern Way) and Class Components(Older Way))
+11. Dive More into React Functional Components.
+12. Component Inside the Component/Nested Component (Component Composition). 
+13. Injecting Plain JS Inside Component Using Curly Brackets { }
+14. 
